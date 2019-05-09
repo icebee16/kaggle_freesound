b156bdba60003c11b81935c4a6dac9319c1bf76d
icebee <nagamine.k.aa@gmail.com>
2019-05-09 15:37:56 +0000

    (0001 Basic WIP)

--- src/0001_Basic.py
+++ src/0001_Basic.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from psutil import cpu_count
 from functools import wraps, partial
 from tqdm import tqdm
+from fastprogress import master_bar, progress_bar
 
 import numpy as np
 import pandas as pd
@@ -21,6 +22,7 @@ from numba import jit
 
 import librosa
 from PIL import Image
+from sklearn.model_selection import train_test_split
 
 import torch
 import torch.nn as nn
@@ -38,7 +40,7 @@ from torchvision.transforms import transforms
 IS_KERNEL = False
 VERSION = "0000" if IS_KERNEL else os.path.basename(__file__)[0:4]
 ROOT_PATH = Path("..") if IS_KERNEL else Path(__file__).parents[1]
-DataLoader = partial(DataLoader, nom_workers=cpu_count())
+DataLoader = partial(DataLoader, num_workers=cpu_count())
 SEED = 1116
 
 # 基礎数値他
@@ -156,41 +158,335 @@ def mono_to_color(mono):
 
 
 # >> dataset section(WIP)
-def df_to_labeldata(df):
+def df_to_labeldata(fpath_arr, labels):
     """
-    df : columns=["fpath", "labels"]
+    fpath   : arr
+    labels  : arr
     """
-    @jit
-    def calc(arr):
-        for idx in tqdm(range(len(arr))):
-            y, sr = read_audio(arr[idx, 0])
+    tag_list = pd.read_csv(ROOT_PATH / "input" / "sample_submission.csv").columns[1:].to_list()  # 80 tag list
+
+    spec_list = []
+    label_list = []
+
+    @jit(parallel=True)
+    def calc(fpath_arr, labels):
+        for idx in tqdm(range(len(fpath_arr))):
+            # melspectrogram
+            y, sr = read_audio(fpath_arr[idx])
             spec_mono = audio_to_melspectrogram(y, sr)
             spec_color = mono_to_color(spec_mono)
-    calc(df.values)
-"""
-    for idx in tqdm(range(len(df))):
-        y, sr = read_audio(df["fpath"].values[idx])
-        spec_mono = audio_to_melspectrogram(y, sr)
-        spec_color = mono_to_color(spec_mono)
-"""
+            spec_list.append(spec_color)
+
+            # labels
+            label = np.zeros(len(tag_list)).astype(int)
+            for tag in labels[idx].split(","):
+                label[tag_list.index(tag)] = 1
+            label_list.append(label)
+
+    calc(fpath_arr, labels)
+
+    return spec_list, label_list
 
 
 class TrainDataset(Dataset):
     """
     train_df :columns=["fpath", "labels"]
     """
-    def __init__(self, train_df, transforms):
+    def __init__(self, train_path_arr, train_y, transform):
         super().__init__()
-        self.transforms = transforms
-        melspectrograms = []
-        labels = []
+        self.transforms = transform
+        self.melspectrograms, self.labels = df_to_labeldata(train_path_arr, train_y)
 
-    def __len__():
-        pass
+    def __len__(self):
+        return len(self.melspectrograms)
 
-    def __getitem__():
-        pass
+    def __getitem__(self, idx):
+        # crop
+        image = Image.fromarray(self.melspectrograms[idx], mode="RGB")
+        time_dim, base_dim = image.size
+        crop = random.randint(0, time_dim - base_dim)
+        image = image.crop([crop, 0, crop + base_dim, base_dim])
+        image = self.transforms(image).div_(255)
+
+        label = self.labels[idx]
+        label = torch.from_numpy(label).float()
+        return image, label
+
+
+def load_testdata(fname):
+    """
+    fname : list
+    """
+    input_dir = ROOT_PATH / "input"
+
+    spec_list = []
+
+    @jit
+    def calc(fname_list):
+        for idx in tqdm(range(len(fname_list))):
+            # melspectrogram
+            y, sr = read_audio(input_dir / "test" / fname_list[idx])
+            spec_mono = audio_to_melspectrogram(y, sr)
+            spec_color = mono_to_color(spec_mono)
+            spec_list.append(spec_color)
+
+    calc(fname)
+
+    return spec_list
+
+
+class TestDataset(Dataset):
+    """
+    """
+    def __init__(self, transform, tta=5):
+        super().__init__()
+        self.transforms = transform
+        self.fnames = pd.read_csv(ROOT_PATH / "input" / "sample_submission.csv")["fname"].values.tolist()
+        self.melspectrograms = load_testdata(self.fnames)
+        self.tta = tta
+
+    def __len__(self):
+        return len(self.melspectrograms) * self.tta
+
+    def __getitem__(self, idx):
+        new_idx = idx % len(self.fnames)
+
+        # crop
+        image = Image.fromarray(self.melspectrograms[new_idx], mode="RGB")
+        time_dim, base_dim = image.size
+        crop = random.randint(0, time_dim - base_dim)
+        image = image.crop([crop, 0, crop + base_dim, base_dim])
+        image = self.transforms(image).div_(255)
+
+        fname = self.fnames[new_idx]
+        return image, fname
 # << dataset section
+
+
+# =============== #
+#  model section  #
+# =============== #
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+
+        self._init_weights()
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.zeros_(m.bias)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = F.avg_pool2d(x, 2)
+        return x
+
+
+class Classifier(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        self.conv = nn.Sequential(
+            ConvBlock(in_channels=3, out_channels=64),
+            ConvBlock(in_channels=64, out_channels=128),
+            ConvBlock(in_channels=128, out_channels=256),
+            ConvBlock(in_channels=256, out_channels=512),
+        )
+
+        self.fc = nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(512, 128),
+            nn.PReLU(),
+            nn.BatchNorm1d(128),
+            nn.Dropout(0.1),
+            nn.Linear(128, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = torch.mean(x, dim=3)
+        x, _ = torch.max(x, dim=2)
+        x = self.fc(x)
+        return x
+
+
+# ================ #
+#  metric section  #
+# ================ #
+# from official code https://colab.research.google.com/drive/1AgPdhSp7ttY18O3fEoHOQKlt_3HJDLi8#scrollTo=cRCaCIb9oguU
+def _one_sample_positive_class_precisions(scores, truth):
+    """Calculate precisions for each true class for a single sample.
+
+    Args:
+      scores: np.array of (num_classes,) giving the individual classifier scores.
+      truth: np.array of (num_classes,) bools indicating which classes are true.
+
+    Returns:
+      pos_class_indices: np.array of indices of the true classes for this sample.
+      pos_class_precisions: np.array of precisions corresponding to each of those
+        classes.
+    """
+    num_classes = scores.shape[0]
+    pos_class_indices = np.flatnonzero(truth > 0)
+    # Only calculate precisions if there are some true classes.
+    if not len(pos_class_indices):
+        return pos_class_indices, np.zeros(0)
+    # Retrieval list of classes for this sample.
+    retrieved_classes = np.argsort(scores)[::-1]
+    # class_rankings[top_scoring_class_index] == 0 etc.
+    class_rankings = np.zeros(num_classes, dtype=np.int)
+    class_rankings[retrieved_classes] = range(num_classes)
+    # Which of these is a true label?
+    retrieved_class_true = np.zeros(num_classes, dtype=np.bool)
+    retrieved_class_true[class_rankings[pos_class_indices]] = True
+    # Num hits for every truncated retrieval list.
+    retrieved_cumulative_hits = np.cumsum(retrieved_class_true)
+    # Precision of retrieval list truncated at each hit, in order of pos_labels.
+    precision_at_hits = (retrieved_cumulative_hits[class_rankings[pos_class_indices]] / (1 + class_rankings[pos_class_indices].astype(np.float)))
+    return pos_class_indices, precision_at_hits
+
+
+def calculate_per_class_lwlrap(truth, scores):
+    """Calculate label-weighted label-ranking average precision.
+
+    Arguments:
+      truth: np.array of (num_samples, num_classes) giving boolean ground-truth
+        of presence of that class in that sample.
+      scores: np.array of (num_samples, num_classes) giving the classifier-under-
+        test's real-valued score for each class for each sample.
+
+    Returns:
+      per_class_lwlrap: np.array of (num_classes,) giving the lwlrap for each
+        class.
+      weight_per_class: np.array of (num_classes,) giving the prior of each
+        class within the truth labels.  Then the overall unbalanced lwlrap is
+        simply np.sum(per_class_lwlrap * weight_per_class)
+    """
+    assert truth.shape == scores.shape
+    num_samples, num_classes = scores.shape
+    # Space to store a distinct precision value for each class on each sample.
+    # Only the classes that are true for each sample will be filled in.
+    precisions_for_samples_by_classes = np.zeros((num_samples, num_classes))
+    for sample_num in range(num_samples):
+        pos_class_indices, precision_at_hits = (
+            _one_sample_positive_class_precisions(scores[sample_num, :],
+                                                  truth[sample_num, :]))
+        precisions_for_samples_by_classes[sample_num, pos_class_indices] = (
+            precision_at_hits)
+    labels_per_class = np.sum(truth > 0, axis=0)
+    weight_per_class = labels_per_class / float(np.sum(labels_per_class))
+    # Form average of each column, i.e. all the precisions assigned to labels in
+    # a particular class.
+    per_class_lwlrap = (np.sum(precisions_for_samples_by_classes, axis=0) / np.maximum(1, labels_per_class))
+    # overall_lwlrap = simple average of all the actual per-class, per-sample precisions
+    #                = np.sum(precisions_for_samples_by_classes) / np.sum(precisions_for_samples_by_classes > 0)
+    #           also = weighted mean of per-class lwlraps, weighted by class label prior across samples
+    #                = np.sum(per_class_lwlrap * weight_per_class)
+    return per_class_lwlrap, weight_per_class
+
+
+# =============== #
+#  train section  #
+# =============== #
+def train_model(train_df, train_transforms):
+    num_epochs = 80
+    batch_size = 64
+    test_batch_size = 256
+    lr = 3e-3
+    eta_min = 1e-5
+    t_max = 10
+
+    num_classes = len(pd.read_csv(ROOT_PATH / "input" / "sample_submission.csv").columns[1:])
+
+    trn_x, val_x, trn_y, val_y = train_test_split(train_df["fpath"].values, train_df["labels"].values, test_size=0.2, random_state=SEED)
+    print(val_y) # (WIP)
+
+    train_dataset = TrainDataset(trn_x, trn_y, train_transforms)
+    valid_dataset = TrainDataset(val_x, val_y, train_transforms)
+    print(val_y)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=test_batch_size, shuffle=False)
+
+    model = Classifier(num_classes=num_classes).cuda()
+    criterion = nn.BCEWithLogitsLoss().cuda()
+    optimizer = Adam(params=model.parameters(), lr=lr, amsgrad=False)
+    scheduler = CosineAnnealingLR(optimizer, T_max=t_max, eta_min=eta_min)
+
+    best_epoch = -1
+    best_lwlrap = 0.
+    mb = master_bar(range(num_epochs))
+
+    for epoch in mb:
+        start = time.time()
+
+        # train
+        model.train()
+        avg_loss = 0.
+
+        for x_batch, y_batch in progress_bar(train_loader, parent=mb):
+            preds = model(x_batch.cuda())
+            loss = criterion(preds, y_batch.cuda())
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            avg_loss += loss.item() / len(train_loader)
+
+        # valid
+        model.eval()
+        valid_preds = np.zeros((len(val_x), num_classes))
+        avg_val_loss = 0.
+        print(val_y)
+
+        for i, (x_batch, y_batch) in enumerate(valid_loader):
+            preds = model(x_batch.cuda()).detach()
+            loss = criterion(preds, y_batch.cuda())
+
+            preds = torch.sigmoid(preds)
+            valid_preds[i * test_batch_size: (i + 1) * test_batch_size] = preds.cpu().numpy()
+
+            avg_val_loss += loss.item() / len(valid_loader)
+
+        print(val_y)
+        score, weight = calculate_per_class_lwlrap(val_y, valid_preds)
+        lwlrap = (score * weight).sum()
+
+        scheduler.step()
+
+        elapsed = time.time() - start
+        get_logger(is_torch=True).debug("{}\t{}\t{}\t{}".format(avg_loss, avg_val_loss, lwlrap, elapsed))
+        if (epoch + 1) % 5 == 0:
+            mb.write(f"Epoch {epoch + 1} - avg_train_loss: {avg_loss:.4f}\tavg_val_loss: {avg_val_loss:.4f}\tval_lwlrap: {lwlrap:.6f}\ttime: {elapsed:.0f}s")
+
+        if lwlrap > best_lwlrap:
+            best_epoch = epoch + 1
+            best_lwlrap = lwlrap
+            torch.save(model.state_dict(), (ROOT_PATH / "model" / "{}_weight_best.pt".format(VERSION)).resolve())
+
+        return {
+            "best_epoch": best_epoch,
+            "best_lwlrap": best_lwlrap
+        }
 
 
 # ================ #
@@ -219,16 +515,17 @@ def create_logger():
         log_dir = ROOT_PATH / "log"
         log_dir.mkdir(exist_ok=True, parents=True)
 
+        # file handler
         _logfile = log_dir / "{}.log".format(VERSION)
         _logfile.touch()
-        fh = FileHandler(_logfile)
+        fh = FileHandler(_logfile, mode="w")
         fh.setLevel(DEBUG)
         fh.setFormatter(fmt)
         _logger.addHandler(fh)
 
         _torch_logfile = log_dir / "{}_torch.log".format(VERSION)
         _torch_logfile.touch()
-        torch_fh = FileHandler(_torch_logfile)
+        torch_fh = FileHandler(_torch_logfile, mode="w")
         torch_fh.setLevel(DEBUG)
         torch_fh.setFormatter("%(message)s")
         _torch_logger.addHandler(torch_fh)
@@ -259,8 +556,24 @@ def main():
     # presetting
     seed_everything()
     jobs_manage()
-    train_df = select_train_data()
-    df_to_labeldata(train_df)
+
+    train_df = select_train_data().iloc[:1000, :]  # WIP
+    train_transforms = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor()
+    ])
+
+    result = train_model(train_df, train_transforms)
+
+    print(result)
+
+    """
+    test_transforms = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor()
+    ])
+    test_ds = TestDataset(test_transforms)
+    """
 
 
 if __name__ == "__main__":
@@ -269,5 +582,5 @@ if __name__ == "__main__":
     try:
         main()
     except Exception as e:
-        print()
         get_logger().error("Exception Occured. \n>> \n {}".format(e))
+        raise e
